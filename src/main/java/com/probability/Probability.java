757d5ac9ec2a5a0e38a75a3052149d6bbf98a9fa
nivetha <nivetha.karthikeyan@gmail.com>
2021-12-03 13:38:14 +0530

    review comments incorporated

--- src/main/java/com/probability/Probability.java
+++ src/main/java/com/probability/Probability.java
@@ -5,30 +5,22 @@ import java.util.Objects;
 public class Probability {
 
     private double value;
-    private final double chancesOfOutcome= 1;
+    private final double CERTAIN_EVENT_VALUE= 1;
 
     public Probability(double value) {
         this.value = value;
     }
 
-    /*
-    * To find the probability of an event not occurring  P(A`) = 1-P(A)
-    * */
+
     public Probability complement() {
-        return new Probability(chancesOfOutcome - this.value);
+        return new Probability(CERTAIN_EVENT_VALUE - this.value);
     }
-    /*
-     * To find the probability of two events occurring together.
-     *  Formula for the probability of A and B (independent events): p(A and B) = p(A) * p(B).
-     * */
+
     public Probability intersection(Probability probabilityOfThat) {
         return new Probability(this.value * probabilityOfThat.value) ;
     }
 
-    /*
-     * To find 4. the probability of either two events occurring.
-     * Formula for the probability of A or B (independent events):p(A or B)=p(A) + p(B) – p(A and B).
-     * */
+
     public Probability union(Probability probabilityOfThat) {
         Probability intersectionProbability = this.intersection(probabilityOfThat);
         return new Probability(this.value + probabilityOfThat.value - intersectionProbability.value) ;
@@ -44,7 +36,7 @@ public class Probability {
 
     @Override
     public int hashCode() {
-        return Objects.hash(value, chancesOfOutcome);
+        return Objects.hash(value, CERTAIN_EVENT_VALUE);
     }
 
     @Override
